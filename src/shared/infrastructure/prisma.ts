cebe6550ca1831a9e119c7ecbefda1df79df614f
MiguelRodac <rodacmig19@gmail.com>
2026-08-13 00:19:31 -0400

    feat: log SQL queries in development mode only

--- src/shared/infrastructure/prisma.ts
+++ src/shared/infrastructure/prisma.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const isDev = process.env.NODE_ENV !== 'production'
+
+export const prisma = new PrismaClient({
+  log: isDev ? ['query', 'error'] : ['error']
+})
 
 export default prisma
